a236d52b0aa557f58a68486fc153dee2df9cf910
vi_ser <vinceulisse@gmail.com>
2024-04-03 16:36:19 +0200

    creazione Navbar ed Header

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<?php
+
+require './Models/Category.php';
+require './Models/Product.php';
+require './Models/Food.php';
+require './Models/Game.php';
+
+
+$cat = new Category("Cat", "cat");
+$dog = new Category("Dog", "dog");
+
+$categories = [
+    $cat,
+    $dog
+];
+
+$products = [
+    new Food("Croccantini premium per cani", 25.99, $dog, "400"),
+    new Game("Palla rimbalzante resistente per cani", 9.99, $dog, "ball"),
+    new Product("Guinzaglio in pelle per cani di grossa taglia", 18.49, $dog),
+    new Food("Scatolette di tonno e salmone per gatti", 1.49, $cat, "200"),
+    new Game("Tunnel giocattolo per gatti", 12.99, $cat, "strutture"),
+    new Product("Lettiera igienica in cristalli per gatti", 7.99, $cat),
+];
+
+// var_dump($categories);
+// var_dump($products);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,6 +47,31 @@
 </head>
 
 <body>
+    <nav class="navbar bg-dark mb-5">
+        <div class="container-fluid">
+            <a class="navbar-brand text-white" href="#">
+                <img src="./img/logo.svg" alt="Logo" width="auto" height="30px" class="d-inline-block align-text-top">
+                Pet Shop
+            </a>
+        </div>
+    </nav>
+    <div class="container p-5">
+
+        <div class="row">
+            <div class="col-6">
+                <h1 class="display-1">
+                    Esplora il Mondo degli Animali Domestici!
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore fugiat atque expedita. Et, labore
+                    aliquid
+                    soluta commodi accusamus ipsam veritatis sequi quasi, error neque voluptatum qui doloremque natus
+                    maxime
+                    vel.</p>
+            </div>
+        </div>
+
+    </div>
+
 
 
     <!-- bootstrap -->
